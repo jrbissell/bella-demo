29f4b16a46b4ae339feaa1a8d72b7b73d2360780
Josh Bissell <joshua.r.bissell@protonmail.com>
2026-06-04 13:48:53 -0500

    Fix WSGI: call create_tables directly, disable a2wsgi lifespan

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,10 @@
 import sys
 sys.path.insert(0, '/home/jrbissell/bella-demo')
 
+from backend.app.database import create_tables
+create_tables()
+
 from a2wsgi import ASGIMiddleware
 from backend.app.main import app
 
-application = ASGIMiddleware(app)
+application = ASGIMiddleware(app, lifespan="off")
